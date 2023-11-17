5bbc9dd45f534784b503306044d97a17e56f6b82
CodingOz <146854126+CodingOz@users.noreply.github.com>
2023-11-17 22:45:21 +0000

    cousework stugg

--- coursework/part2/task2/StepCounter.c
+++ coursework/part2/task2/StepCounter.c
@@ -9,34 +9,61 @@ typedef struct {
 	int steps;
 } FITNESS_DATA;
 
+
 int main()
 {
 
     while (1==1)
     {
         char choice; 
-        printf("A: Specify the filename to be imported – you need to check that the file opened correctly.\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the data and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Exit.\n");
-        scanf("%c", &choice);
+        printf("A: open file\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the data and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Exit.\n");
+        scanf("%s", &choice);
+
+        //variables for A
+        char filename[200];
+
+        //variables for B 
+        FITNESS_DATA datalist[500];
+        int Buffer_size = 200;
+        char Buffer_for_lines[Buffer_size];
+        int count = 0;
+
+
         //choice = toupper(choice);
         switch (choice)
         {
         case 'A':
-            printf("aaaaaaaaa\n");
-            char *filename;
-            printf("input filename:");
-            scanf("%c", filename);
+            printf("input filename: ");
+            scanf("%s", filename);
             FILE *file = fopen(filename, "r");
             if (file == NULL)
             {
-                printf("Error: could not open file");
+                printf("Error: could not open file\n");
                 return 1;
             }
             break;
+        
         case 'B':
-            printf("b");
+            count = 0;
+            while (fgets(Buffer_for_lines,Buffer_size,file))
+            {
+                count++;
+            }
+            printf("Total records: %i\n", count);
+            file = fopen(filename, "r");
+            if (file == NULL)
+            {
+                printf("Error: could not open file\n");
+                return 1;
+            }
+
             break;
         case 'C':
-            printf("c");
+            while (fgets(Buffer_for_lines,Buffer_size,file))
+            {
+                
+            }
+            
             break;
         case 'D':
             printf("d");
@@ -51,6 +78,7 @@ int main()
             return 0;
             break;
         default:
+            printf("not recognised\n");
             break;
         }
     }
