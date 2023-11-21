717f9512f9a7c23bff8abd9fd7d2094f8f0a5cdb
CodingOz <146854126+CodingOz@users.noreply.github.com>
2023-11-21 16:45:21 +0000

    jst need to do the rounding for tesk 2

--- coursework/part2/task2/StepCounter.c
+++ coursework/part2/task2/StepCounter.c
@@ -2,50 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 // Define an appropriate struct
 typedef struct {
 	char date[11];
 	char time[6];
 	int steps;
 } FITNESS_DATA;
-
-char tokenise(FILE *file){
-    int count = 0;
-    char *tempdate;
-    char *temptime;
-    char *tempsteps;
-    FITNESS_DATA templist[500];
-    int Buffer_size = 200;
-    char Buffer_for_lines[Buffer_size];
-    int *count = 0;
-    while (fgets(Buffer_for_lines,Buffer_size,file))
-    {
-    char *inputCopy = strdup(Buffer_for_lines);
-    char *token = strtok(inputCopy, ",");
-    if (token != NULL)
-    {
-        tempdate = token; 
-    }
-    token = strtok(NULL, ",");
-    if (token != NULL) {
-        temptime = token;
-    }
-    token = strtok(NULL, ",");
-    if (token != NULL) {
-        tempsteps = token;
-    }
-
-    strcpy(templist[count].date, tempdate);
-    strcpy(templist[count].time, temptime);
-    int tempstep = atoi(tempsteps);
-    templist[count].steps = tempstep;
-
-    count++;
-    }
-    return templist;
-
-}
-
 
 
 int main()
@@ -59,24 +22,39 @@ int main()
 
         //variables for A
         char filename[200];
-
-        //variables for B 
+        char *tempdate;
+        char *temptime;
+        char *tempsteps;
         int Buffer_size = 200;
         char Buffer_for_lines[Buffer_size];
         int count = 0;
-        //variables for C
+        int len;
         FITNESS_DATA datalist[500];
-        FITNESS_DATA *templist[500];
-        char *date;
-        char *time;
-        char *steps;
-        int length;
+        //variables for C
         int lowest;
         char *lowest_date;
         char *lowest_time;
+        //variables for D
+        int highest;
+        char *highest_date;
+        char *highest_time;
+        //variables for E
+        float total;
+        float tempaverage;
+        float average;
+        //variables for F
+        int best;
+        int currant;
+        
+        char *startdate;
+        char *starttime;
+        char *enddate;
+        char *endtime;
+        char *currantstartdate;
+        char *currantstarttime;
+        char *currantenddate;
+        char *currantendtime;
 
-
-        //choice = toupper(choice);
         switch (choice)
         {
         case 'A':
@@ -90,6 +68,35 @@ int main()
                 printf("Error: could not open file\n");
                 return 1;
             }
+            count = 0;
+            //creats arrey with file values for later options B, C and D
+            while (fgets(Buffer_for_lines,Buffer_size,file))
+            {
+                char *inputCopy = strdup(Buffer_for_lines);
+                char *token = strtok(inputCopy, ",");
+                if (token != NULL)
+                {
+                    tempdate = token; 
+                }
+                token = strtok(NULL, ",");
+                if (token != NULL) {
+                    temptime = token;
+                }
+                token = strtok(NULL, ",");
+                if (token != NULL) {
+                    tempsteps = token;
+                }
+
+                strcpy(datalist[count].date, tempdate);
+                strcpy(datalist[count].time, temptime);
+                int tempstep = atoi(tempsteps);
+                datalist[count].steps = tempstep;
+                count++;
+            }
+            len = count;
+            //reopens file
+            file = fopen(filename, "r");
+
             break;
         
         case 'B':
@@ -99,7 +106,8 @@ int main()
             {
                 count++;
             }
-            printf("Total records: %i\n", count);
+            len = count;
+            printf("Total records: %i\n", len);
             //reopens file
             file = fopen(filename, "r");
             break;
@@ -107,48 +115,18 @@ int main()
         case 'C':
         case 'c':
             count = 0;
-            templist = tokenise(file);
-            /*while (fgets(Buffer_for_lines,Buffer_size,file))
-            {
-                char *inputCopy = strdup(Buffer_for_lines);
-                char *token = strtok(inputCopy, ",");
-                if (token != NULL)
-                {
-                date = token; 
-                }
-                token = strtok(NULL, ",");
-                if (token != NULL) {
-                    time = token;
-                }
-                token = strtok(NULL, ",");
-                if (token != NULL) {
-                    steps = token;
-                }
-
-                strcpy(datalist[count].date, date);
-                strcpy(datalist[count].time, time);
-                int tempstep = atoi(steps);
-                datalist[count].steps = tempstep;
-
-                count++;
-            }
-            */
-
-            //length = sizeof(array);
-            lowest = datalist[0].steps;
+            lowest = datalist[count].steps;
             lowest_date = datalist[0].date;
             lowest_time = datalist[0].time;            
-            for (count = 0; count < length; count++)
+            for (count = 0; count < len; count++)
             {
                 if (datalist[count].steps < lowest){
                     lowest = datalist[count].steps;
                     lowest_date = datalist[count].date;
                     lowest_time = datalist[count].time;
                 }
-
             }
             printf("Fewest steps: %s %s\n", lowest_date, lowest_time);
-
             //reopens file
             fclose(file);
             file = fopen(filename, "r");
@@ -156,15 +134,68 @@ int main()
 
         case 'D':
         case 'd':
-            printf("d");
+            count = 0;
+            highest = -1;
+            highest_date = datalist[0].date;
+            highest_time = datalist[0].time;            
+            for (count = 0; count < len; count++)
+            {
+                if (datalist[count].steps > highest){
+                    highest = datalist[count].steps;
+                    highest_date = datalist[count].date;
+                    highest_time = datalist[count].time;
+                }
+            }
+            printf("Largest steps: %s %s\n", highest_date, highest_time);
+            //reopens file
+            fclose(file);
+            file = fopen(filename, "r");
             break;
         case 'E':
         case 'e':
-            printf("e");
+            total = 0;
+            for (count = 0; count < len; count++)
+            {
+                total = total + datalist[count].steps;
+            }
+            average = total/len;
+            tempaverage = 110/3.0;
+
+            printf("Mean step count: %f\n", average);
+            //reopens file
+            fclose(file);
+            file = fopen(filename, "r");
             break;
         case 'F':
         case 'f':
-            printf("f");
+            best = 0;
+            currant = 0;
+            for (count = 0; count < len; count++)
+            {
+                if (datalist[count].steps>500){
+                    if (currant == 0){
+                        currantstartdate = datalist[count].date;
+                        currantstarttime = datalist[count].time;
+                    }
+                    currant++;
+                }
+                else{
+
+                    currantenddate = datalist[count-1].date;
+                    currantendtime = datalist[count-1].time;
+
+                    if (currant>best){
+                        best = currant;
+                        startdate = currantstartdate;
+                        starttime = currantstarttime;
+                        enddate= currantenddate;
+                        endtime = currantendtime;
+                    }
+                    currant = 0;
+                }
+            }
+            printf("Longest period start: %s %s\n", startdate, starttime);
+            printf("Longest period end: %s %s\n", enddate, endtime);
             break;
         case 'Q':
         case 'q':
